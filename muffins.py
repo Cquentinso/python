8e678c6d8a155ea82c438bcf7ff60859d2fe90fd
Cquentinso <quentinchristian60@gmail.com>
2024-09-13 15:45:13 -0500

    Add files via upload

---
+++ muffins.py
@@ -0,0 +1,17 @@
+muffin=2
+cupcakes=2
+i=0
+
+for i in range(4):
+    choice=input(str('Do you want a muffin or a cupcake?'))
+    if choice=='muffin':
+        muffin= muffin-1
+    elif choice=='cupcakes':
+        cupcakes= cupcakes-1
+i=i+1
+if cupcakes==0 and muffin==0:
+    print('out of stock')
+
+finish=input('press 0 to finish ')
+print ('cupcakes:',cupcakes)
+print('muffins:',muffin)
